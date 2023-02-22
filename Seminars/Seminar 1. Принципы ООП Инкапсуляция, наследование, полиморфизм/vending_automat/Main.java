8642ba473331434f5918548a36451497723a4150
Dmitry <burmitr@yandex.ru>
2023-02-23 01:20:30 +0500

    sem1

---
+++ Seminars/Seminar 1. Принципы ООП Инкапсуляция, наследование, полиморфизм/vending_automat/Main.java
@@ -0,0 +1,9 @@
+package vending_automat;
+
+public class Main {
+    public static void main(String[] args) {
+        Product cola = new Product("Cola", 23);
+        
+    
+    }
+}
---
+++ Seminars/Seminar 1. Принципы ООП Инкапсуляция, наследование, полиморфизм/vending_automat/VendingAutomat.java
@@ -0,0 +1,12 @@
+package vending_automat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VendingAutomat {
+    private List<Product> goods = new ArrayList<>();
+
+    public void initProducts(List<Product> goods){
+        
+    }
+}
